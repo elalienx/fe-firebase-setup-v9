6123118f0824b90f626c146489ab6d51f026a94a
Eduardo <el_alienx@hotmail.com>
2021-10-06 08:08:37 +0200

    installed firebase npm

--- src/components/TodoOnClass.tsx
+++ src/components/TodoOnClass.tsx
@@ -4,6 +4,9 @@ export default function TodoOnClass() {
       <h2>To do on Class</h2>
       <ol>
         <li>Connect Firebase V9 SDK to project</li>
+        <li>
+          Install Firebase NPM <code>npm install firebase</code>
+        </li>
         <li>Create FireStore database</li>
         <li>Add some documents to the database</li>
         <li>Play with the C.R.U.D methods</li>
