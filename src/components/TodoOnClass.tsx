43f2a5633270e7c0251ce8270096173320e5ea6a
Eduardo <el_alienx@hotmail.com>
2021-10-06 08:06:16 +0200

    first public commit

--- src/components/TodoOnClass.tsx
+++ src/components/TodoOnClass.tsx
@@ -2,11 +2,12 @@ export default function TodoOnClass() {
   return (
     <section>
       <h2>To do on Class</h2>
-      <ul>
+      <ol>
         <li>Connect Firebase V9 SDK to project</li>
         <li>Create FireStore database</li>
+        <li>Add some documents to the database</li>
         <li>Play with the C.R.U.D methods</li>
-      </ul>
+      </ol>
     </section>
   );
 }
